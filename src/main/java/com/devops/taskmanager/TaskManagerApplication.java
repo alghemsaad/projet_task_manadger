0b8de0811799db634afe5e0ed7b8c67d79c6beae
Saad Alghem <alghem.saad.dev@gmail.com>
2026-07-14 23:12:02 +0100

    Prepare Spring Boot JSP app for deployment

--- src/main/java/com/devops/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/devops/taskmanager/TaskManagerApplication.java
@@ -1,27 +1,21 @@
 package com.devops.taskmanager;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskManagerApplication {
+public class TaskManagerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application
+    ) {
+        return application.sources(TaskManagerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
-    }
-
-    @Configuration
-    public class WebConfig implements WebMvcConfigurer {
-        @Autowired
-        private SecurityInterceptor securityInterceptor;
-
-        @Override
-        public void addInterceptors(InterceptorRegistry registry) {
-            registry.addInterceptor(securityInterceptor);
-        }
     }
 }
